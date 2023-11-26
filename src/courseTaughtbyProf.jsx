c2163ffcf9cb164f7b5936a4845e23da5dfc5fa6
floatsinkfc <floatsinkfc@gmail.com>
2023-11-26 15:14:13 -0600

    profName fix

--- src/courseTaughtbyProf.jsx
+++ src/courseTaughtbyProf.jsx
@@ -7,12 +7,13 @@ const ProfessorCourses = () => {
 
   const professorCourses = coursesData.reduce((acc, course) => {
     const coursesTaught = Object.entries(course.classes).reduce((classList, [classNum, classInfo]) => {
-      Object.entries(classInfo.professor).forEach(([profID]) => {
+      Object.entries(classInfo.professor).forEach(([profID, professor]) => {
         if (profID.includes(professorID)) {
           classList.push({
             courseID: course.id,
             classNum: classNum,
             professorID: professorID,
+            professorName: professor.professorName,
             className: classInfo.name,
           });
         }
@@ -27,10 +28,10 @@ const ProfessorCourses = () => {
       <div className="w-full min-h-screen overflow-hidden">
         <div className="flex flex-wrap justify-center px-[20%] gap-5 mt-16 mb-10">
           <div className="w-full text-center items-center">
-            <h2 className="text-2xl text-ChicagoBlue">{professorID}'s Courses</h2>
+            <h2 className="text-2xl text-ChicagoBlue">{professorCourses.length > 0 ? professorCourses[0].professorName : ''}'s Courses</h2>
           </div>
           <div className="w-full mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {professorCourses.map(({ courseID, classNum, professorID, className }) => (
+            {professorCourses.map(({ courseID, classNum, professorID, professorName, className }) => (
               <div key={classNum} className="border p-4">
                 <Link to={`/courses/${courseID}/${classNum}/${professorID}`}>
                   <p className="font-bold">{classNum}</p>
